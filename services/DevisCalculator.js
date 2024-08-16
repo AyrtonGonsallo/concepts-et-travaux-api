11f1f837b9bd43ba720226cd5ea4f5804ff850e2
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2024-08-16 16:30:02 +0100

    correction calculs

--- services/DevisCalculator.js
+++ services/DevisCalculator.js
@@ -118,10 +118,14 @@ class DevisCalculator {
         let etat_surfaces = donnees_json["etat-surfaces-pose-murs"].murs;
         let total=dimensions.length
         for(let i=0;i<total;i++){
-          prix+=dimensions[i].surface*gammes[i].peinture
+          /* prix+=dimensions[i].surface*gammes[i].peinture
           prix+=dimensions[i].surface*gammes[i].carrelage
           prix+=dimensions[i].surface*gammes[i].papier
-          prix+=dimensions[i].surface*gammes[i].enduit
+          prix+=dimensions[i].surface*gammes[i].enduit */
+          let id_prix_gamme=gammes[i].gamme
+          let gammeParts = id_prix_gamme.split(':');  // Sépare la chaîne en deux parties
+          let prix_gamme = parseFloat(gammeParts[1]); 
+          prix+=dimensions[i].surface*prix_gamme
         }
         return prix
       }
@@ -160,21 +164,25 @@ class DevisCalculator {
         get_prix_tache_8(donnees_json){
           let prix=0
           let surface=donnees_json["dimensions-pose-plafond"].surface
-          let prix_carrelage=donnees_json["gammes-produits-pose-plafond"].carrelage;
+          let id_prix_gamme=donnees_json["gammes-produits-pose-plafond"].gamme;
+       /*    let prix_carrelage=donnees_json["gammes-produits-pose-plafond"].carrelage;
+          
           let prix_papier=donnees_json["gammes-produits-pose-plafond"].papier;
           let prix_enduit=donnees_json["gammes-produits-pose-plafond"].enduit;
-          let prix_peinture=donnees_json["gammes-produits-pose-plafond"].peinture;
-          prix+=prix_carrelage*surface
+          let prix_peinture=donnees_json["gammes-produits-pose-plafond"].peinture; */
+         /*  prix+=prix_carrelage*surface
           prix+=prix_papier*surface
           prix+=prix_enduit*surface
-          prix+=prix_peinture*surface
-            
+          prix+=prix_peinture*surface */
+          let gammeParts = id_prix_gamme.split(':');  // Sépare la chaîne en deux parties
+          let prix_gamme = parseFloat(gammeParts[1]); 
+          prix+=prix_gamme*surface
           return prix
         }
         get_prix_tache_9(donnees_json){
           let prix=0
           let surface=donnees_json["dimensions-pose-sol"].surface
-          let prix_plinthes=donnees_json["gammes-produits-pose-sol"].plinthes;
+         /*  let prix_plinthes=donnees_json["gammes-produits-pose-sol"].plinthes;
           let has_pvc=(donnees_json["gammes-produits-pose-sol"].sol_pvc)?1:0;
           let has_moquette=(donnees_json["gammes-produits-pose-sol"].moquette)?1:0;
           let prix_pvc=donnees_json["gammes-produits-pose-sol"].sol_pvc_prix;
@@ -183,14 +191,23 @@ class DevisCalculator {
           let prix_parquet_massif=donnees_json["gammes-produits-pose-sol"].parquet_massif;
           let prix_paquet_flottant_finition_bois=donnees_json["gammes-produits-pose-sol"].paquet_flottant_finition_bois;
           let prix_parquet_flottant_finition_stratifiee=donnees_json["gammes-produits-pose-sol"].parquet_flottant_finition_stratifiee;
-          prix+=prix_plinthes*surface
+           */
+          let id_prix_gamme=donnees_json["gammes-produits-pose-sol"].gamme;
+          let id_prix_plinthes=donnees_json["gammes-produits-pose-sol"].plinthes;
+          let gammeParts = id_prix_gamme.split(':');  // Sépare la chaîne en deux parties
+          let prix_gamme = parseFloat(gammeParts[1]); 
+          let plinthesParts = id_prix_gamme.split(':');  // Sépare la chaîne en deux parties
+          let prix_plinthes = parseFloat(plinthesParts[1]); 
+          /* prix+=prix_plinthes*surface
           prix+=prix_carrelage*surface
           prix+=prix_parquet_massif*surface
           prix+=has_moquette*prix_moquette
           prix+=has_pvc*prix_pvc
           prix+=prix_paquet_flottant_finition_bois*surface
-          prix+=prix_parquet_flottant_finition_stratifiee*surface
-            
+          prix+=prix_parquet_flottant_finition_stratifiee*surface */
+          prix+=prix_gamme*surface
+          prix+=prix_plinthes*surface
+
           return prix
         }
         
@@ -203,19 +220,36 @@ class DevisCalculator {
           let total=dimensions.length
           for(let i=0;i<total;i++){
             let surface=dimensions[i].surface
-            let has_carrelage=(gammes[i].carrelage)?1:0;
+            /* let has_carrelage=(gammes[i].carrelage)?1:0;
             let has_papier=(gammes[i].papier)?1:0;
             let has_enduit=(gammes[i].enduit)?1:0;
             let has_peinture=(gammes[i].peinture)?1:0;
             let has_lambris=(gammes[i].lambris)?1:0;
-            let has_tissus=(gammes[i].tissus)?1:0;
-      
-            prix+=has_carrelage*surface*this.tache_retirer_carrelage.Prix
-            prix+=has_papier*surface*this.tache_retirer_papier.Prix
-            prix+=has_peinture*surface*this.tache_retirer_peinture.Prix
-            prix+=has_enduit*surface*this.tache_retirer_enduit.Prix
-            prix+=has_lambris*surface*this.tache_retirer_lambris.Prix
-            prix+=has_tissus*surface*this.tache_retirer_tissus.Prix
+            let has_tissus=(gammes[i].tissus)?1:0; */
+            let gamme=gammes[i].gamme;
+
+            if(gamme=="Peinture"){
+              prix+=surface*this.tache_retirer_peinture.Prix
+            }else if(gamme=="Enduit decoratif"){
+              prix+=surface*this.tache_retirer_enduit.Prix
+            }else if(gamme=="Papier peint"){
+              prix+=surface*this.tache_retirer_papier.Prix
+            }else if(gamme=="Carrelage mural"){
+              prix+=surface*this.tache_retirer_carrelage.Prix
+            }else if(gamme=="tissus"){
+              prix+=surface*this.tache_retirer_tissus.Prix
+            }else if(gamme=="lambris mural"){
+              prix+=surface*this.tache_retirer_lambris.Prix
+            }
+            else if(gamme=="autre"){
+              prix+=surface*20
+            }
+            
+            
+            
+            
+            
+            
           }
             
           return prix
